34311bdf087d91571a1fd107bad79d3afcdf089b
AkashiTakumi <akataku3025@gmail.com>
2021-06-15 12:48:32 +0900

    数あてゲームver.1.1

--- kazuate/src/Keyboard.java
+++ kazuate/src/Keyboard.java
@@ -6,7 +6,13 @@ public class Keyboard {
 
         System.out.println("数を入力");
         int number = scanner.nextInt();
+        if (number < 10 || 100 <= number) {
+            System.out.println("もう一度二桁で数を入力してください");
+            return inputNumber();
+        } else {
+            return number;
+        }
 
-        return number;
+        //return number;
     }
 }
